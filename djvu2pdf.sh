7021c13812f3e6630c98c2a1d1ea809592f83db8
graip <graip@users.noreply.github.com>
2016-03-01 15:46:49 +0100

    Update djvu2pdf.sh
    Installation check added

--- djvu2pdf.sh
+++ djvu2pdf.sh
@@ -1,6 +1,37 @@
 #!/bin/bash
 
+# Check if djvups and ps2pdf are installed, otherwise prompt
+
+if ! hash djvups 2>/dev/null; then
+	read -p "djvups not installed, proceed with installation? " -n 1 -r
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		echo -e "Installing djvups...\n"
+		gksudo apt-get install djvups
+	else
+		echo "Exiting."
+		exit 1
+	fi
+fi
+	
+if ! hash ps2pdf 2>/dev/null; then
+	read -p "ps2pdf not installed, proceed with installation? " -n 1 -r
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		echo -e "Installing ps2pdf...\n"
+		gksudo apt-get install ps2pdf
+	else
+		echo "Exiting."
+		exit 1
+	fi
+fi
+
+# Convert file
+
 file=$(basename "$1")
+#extension="${file##*.}"
 filename="${file%.*}"
 
 djvups "$1" temp.ps
